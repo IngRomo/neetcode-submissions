a62fa0849337e3d262b90e7e8cc4748cd731cfb2
Romo <emiliorova@gmail.com>
2026-05-02 23:41:38 -0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int size = temperatures.size();
+        vector<int> waitedDays(size,0);
+        stack<int> s;
+
+        for (int index = 0; index < size; index++) {
+            while (!s.empty() && temperatures[index] > temperatures[s.top()]) { //* stack not empty & this temp > last temp
+                int prevIndex = s.top();
+                s.pop();
+                waitedDays[prevIndex] = index - prevIndex;
+            }
+            s.push(index);
+        }
+
+        return waitedDays;
+    }
+};
